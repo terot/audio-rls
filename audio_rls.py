1cc01cc0d38b91bbdf5ec721c80ded817818f3bd
Tero Tapiovaara <tero@terotapiovaara.com>
2018-08-22 11:53:57 +0300

    clarified code

--- audio_rls.py
+++ audio_rls.py
@@ -5,23 +5,9 @@ import scipy.io.wavfile as wav
 import numpy as np
 from rls import RLS
 
-def parse_positive_int(value):
-    ivalue = int(value)
-    if ivalue <= 0:
-         raise argparse.ArgumentTypeError("{} is an invalid value for positive int".format(value))
-    return ivalue
-
-def parse_restricted_float(value):
-    fvalue = float(value)
-    if fvalue < 0 or fvalue > 1:
-         raise argparse.ArgumentTypeError("{} is not in range [0,1]".format(value))
-    return fvalue
-
-def parse_positive_float(value):
-    fvalue = float(value)
-    if fvalue <= 0:
-        raise argparse.ArgumentTypeError("{} is an invalid value for positive float".format(value))
-    return fvalue
+##
+# Argument parsing
+#
 
 def get_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -33,52 +19,81 @@ def get_args():
     # optional arguments
     parser.add_argument('--dim_input', type=int,
                         default=1000, help="The input window size")
-    parser.add_argument('--print_every', type=parse_positive_int,
+    parser.add_argument('--print_every', type=positive_int,
                         default=1000, help="Report average squared error every nth sample")
-    parser.add_argument('--prediction_distance', type=parse_positive_int,
+    parser.add_argument('--prediction_distance', type=positive_int,
                         default=1, help="How far into future to predict") # >=1
     parser.add_argument('--prediction_output', type=str,
                         default=None, help="Write predicted data to wav file")
     parser.add_argument('--error_output', type=str,
                         default=None, help="Write prediction error to wav file")
-    parser.add_argument('--rls_lambda', type=parse_restricted_float,
+    parser.add_argument('--rls_lambda', type=restricted_float,
                         default=1.0, help="RLS lambda parameter, aka. forgetting factor")
-    parser.add_argument('--rls_delta', type=parse_positive_float,
+    parser.add_argument('--rls_delta', type=positive_float,
                         default=0.001, help="RLS delta parameter for approximate initialization")
     parser.add_argument('--verbose', action="store_true",
                         help="Verbose printing")
+
     return parser.parse_args()
+
+def positive_int(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+         raise argparse.ArgumentTypeError("{} is an invalid value for positive int".format(value))
+    return ivalue
+
+def restricted_float(value):
+    fvalue = float(value)
+    if fvalue < 0 or fvalue > 1:
+         raise argparse.ArgumentTypeError("{} is not in range [0,1]".format(value))
+    return fvalue
+
+def positive_float(value):
+    fvalue = float(value)
+    if fvalue <= 0:
+        raise argparse.ArgumentTypeError("{} is an invalid value for positive float".format(value))
+    return fvalue
+
+##
+# Main
+#
 
 def main(args):
   if args.verbose:
     print("args: {}".format(vars(args)))
 
+  # init model
   rls = RLS(args.dim_input, args.rls_delta)
-  sample_rate, input_data = wav.read(args.input_file)
 
+  # load data
+  sample_rate, input_data = wav.read(args.input_file)
   if args.verbose:
     print("sample rate: {} Hz".format(sample_rate))
     print("num samples: {}".format(input_data.size))
 
+  # number of samples to predict
   tot_size = input_data.size - args.dim_input - args.prediction_distance + 1
 
-  output_data = np.array([], dtype=float)
-  error_data = np.array([], dtype=float)
-  error_sqr_sum = 0.0
+  # init predicted output and error arrays to empty
+  outputs = np.array([], dtype=float)
+  errors = np.array([], dtype=float)
 
+  error_sqr_sum = 0.0
   for t in range(tot_size):
 
-    if t%args.print_every==0:
+    if t>0 and t%args.print_every==0:
       print("{}%\t{}".format(round(100.0*t/tot_size,2), round(error_sqr_sum/args.print_every,5)))
       error_sqr_sum = 0.0
 
+    # compute predicted output and error, and train model
     input = input_data[t:t+args.dim_input]/32767.0
     target = input_data[t+args.dim_input+args.prediction_distance-1]/32767.0
     output, error = rls.observe(input, target, args.rls_lambda)
     error_sqr_sum += error*error
 
-    output_data = np.append(output_data, output)
-    error_data = np.append(error_data, error)
+    # append predicted output and error to corresponding arrays
+    outputs = np.append(outputs, output)
+    errors = np.append(errors, error)
 
   # write prediction output wav file
   if args.prediction_output is not None:
--- rls.py
+++ rls.py
@@ -4,32 +4,42 @@ import theano.tensor as T
 
 class RLS:
   def __init__(self, order, gamma):
-    weight_value = np.zeros(order,
-                           dtype = theano.config.floatX)
-    P_value = 1.0/gamma*np.eye(order,
-                               dtype=theano.config.floatX)
-    weight = theano.shared(value = weight_value,
-                           name = 'weight',
-                           borrow = True)
-    P = theano.shared(value = P_value,
-                      name='P',
-                      borrow=True)
-        
+    weight = self.initial_weight(order)
+    P = self.initial_P(order, gamma)
+
+    # setup inputs
     input = T.vector("input")
     target = T.dscalar('target')
     lmbda = T.dscalar('lmbda')
-    
+
+    # setup outputs
     output = T.dot(input, weight)
     error = target - output
+
+    # setup updates
     xP = T.dot(P.T,input) # input.T*P (vector)
     a = 1.0/(lmbda + T.dot(xP, input)) # scalar
     g = T.dot(P, input)*a # P*input*a (vector)
     new_weight = weight + error*g
     newP = (1.0/lmbda) * (P - T.outer(g,xP))
     
+    # observe computes outputs and performs update for learning
     self.observe = theano.function(inputs = [input, target, lmbda],
                                    outputs = [output, error],
                                    updates = [
                                       (weight, new_weight),
                                       (P, newP)
                                    ])
+  def initial_weight(self, order):
+    weight_value = np.zeros(order,
+                            dtype = theano.config.floatX)
+    return theano.shared(value = weight_value,
+                         name = 'weight',
+                         borrow = True)
+
+  def initial_P(self, order, gamma):
+    P_value = 1.0/gamma*np.eye(order,
+                               dtype=theano.config.floatX)
+    return theano.shared(value = P_value,
+                         name='P',
+                         borrow=True)
